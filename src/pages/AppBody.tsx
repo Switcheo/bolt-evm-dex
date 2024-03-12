c66058f98b9fa5000faac3045305c681b768d14f
Fabian Chua <fabianchua6@gmail.com>
2024-03-12 16:49:43 +0800

    Update AppBody styling

--- src/pages/AppBody.tsx
+++ src/pages/AppBody.tsx
@@ -3,11 +3,14 @@ import styled from "styled-components";
 export const BodyWrapper = styled.div`
   position: relative;
   max-width: 420px;
+  margin-top: auto;
+  margin-bottom: auto;
   width: 100%;
-  background: ${({ theme }) => theme.bg1};
+  border: ${({ theme }) => theme.border1};
+  background: ${({ theme }) => theme.glassBg};
   box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.01), 0px 4px 8px rgba(0, 0, 0, 0.04), 0px 16px 24px rgba(0, 0, 0, 0.04),
     0px 24px 32px rgba(0, 0, 0, 0.01);
-  border-radius: 30px;
+  border-radius: 10px;
 `;
 
 /**
