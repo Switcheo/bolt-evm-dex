fc593c2fdfa31d25feb63b32a15c7f31b985151d
Bryan Lee <leebryan307@gmail.com>
2024-09-23 18:00:16 +0800

    Update prepareWriteContract to simulateContract

--- src/hooks/useWrapCallback.ts
+++ src/hooks/useWrapCallback.ts
@@ -1,8 +1,9 @@
 import { TransactionResponse } from "@ethersproject/providers";
 import { useMemo } from "react";
 import { parseUnits, TransactionReceipt } from "viem";
-import { useAccount } from "wagmi";
-import { prepareWriteContract, waitForTransaction, writeContract } from "wagmi/actions";
+import { createConfig, http, useAccount } from "wagmi";
+import { simulateContract, writeContract } from "wagmi/actions";
+import { waitForTransactionReceipt } from '@wagmi/core'
 import { wethABI } from "../constants/abis";
 import { WETH_ADDRESSES } from "../constants/addresses";
 import { WETH_TOKENS } from "../constants/tokens";
@@ -12,6 +13,8 @@ import { CurrencyAmount } from "../utils/entities/fractions/currencyAmount";
 import { TokenAmount } from "../utils/entities/fractions/tokenAmount";
 import { currencyEquals, Token } from "../utils/entities/token";
 import { useCurrencyBalance } from "./balances/useCurrencyBalance";
+import { getDefaultConfig } from "connectkit";
+import { sepolia as baseSepolia } from "wagmi/chains";
 
 export enum WrapType {
   NOT_APPLICABLE,
@@ -62,20 +65,58 @@ interface WrapCallbackArgs {
       | undefined,
   ) => void;
 }
+const walletConnectProjectId = import.meta.env.WALLET_CONNECT_PROJECT_ID;
+const pivotal = {
+  id: 16481,
+  name: "Pivotal Sepolia",
+  network: "Pivotal Sepolia",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Ethereum",
+    symbol: "ETH",
+  },
+  rpcUrls: {
+    public: { http: ["https://sepolia.pivotalprotocol.com"] },
+    default: { http: ["https://sepolia.pivotalprotocol.com"] },
+  },
+  blockExplorers: {
+    default: { name: "Blockscout", url: "https://sepolia.pivotalscan.xyz" },
+  },
+};
+const sepolia = {
+  ...baseSepolia,
+  rpcUrls: {
+    ...baseSepolia.rpcUrls,
+    default: { http: ["https://eth-sepolia.g.alchemy.com/v2/7y0_VO9hXrNuU0iroPAPEnhFkDX13XY2"] },
+    public: { http: ["https://eth-sepolia.g.alchemy.com/v2/7y0_VO9hXrNuU0iroPAPEnhFkDX13XY2"] },
+  }
+}
+
+const config = createConfig(
+  getDefaultConfig({
+    appName: "Pivotal Swap",
+    walletConnectProjectId,
+    transports: {
+      [pivotal.id]: http("https://sepolia.pivotalprotocol.com"),
+      [sepolia.id]: http('https://eth-sepolia.g.alchemy.com/v2/7y0_VO9hXrNuU0iroPAPEnhFkDX13XY2'),
+    },
+    chains: [pivotal, sepolia],
+  }),
+);
 
 const wrapEther = async ({ inputAmount, chainId, addTransaction }: WrapCallbackArgs) => {
-  const data = await prepareWriteContract({
+  const { request } = await simulateContract(config , {
     address: WETH_ADDRESSES[chainId],
     abi: wethABI,
     functionName: "deposit",
     value: BigInt(inputAmount.raw.toString()),
-  });
-  const { hash } = await writeContract(data.request);
+  })
+  const hash = await writeContract(config ,request);
 
-  const transactionReceipt = await waitForTransaction({
-    hash,
+  const transactionReceipt = await waitForTransactionReceipt(config, {
     chainId,
-  });
+    hash
+  })
 
   addTransaction(transactionReceipt, {
     summary: `Wrap ${inputAmount.toSignificant(6)} ETH to WETH`,
@@ -83,18 +124,18 @@ const wrapEther = async ({ inputAmount, chainId, addTransaction }: WrapCallbackA
 };
 
 const unwrapEther = async ({ inputAmount, chainId, addTransaction }: WrapCallbackArgs) => {
-  const data = await prepareWriteContract({
+  const { request } = await simulateContract(config, {
     address: WETH_ADDRESSES[chainId],
     abi: wethABI,
     functionName: "withdraw",
     args: [BigInt(inputAmount.raw.toString())],
-  });
-  const { hash } = await writeContract(data.request);
+  })
+  const hash = await writeContract(config, request);
 
-  const transactionReceipt = await waitForTransaction({
-    hash,
+  const transactionReceipt = await waitForTransactionReceipt(config, {
     chainId,
-  });
+    hash
+  })
 
   addTransaction(transactionReceipt, { summary: `Wrap ${inputAmount.toSignificant(6)} ETH to WETH` });
 };
